7ba980e148d1d8bce285b6eb711490579d4528fe
Tamish Patel <tpatel29@horizon.csueastbay.edu>
2020-09-01 14:25:30 -0700

    Updated the javascript code

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
 //generating random number for dice-1
-var randomNumber1 = Math.random();
-randomNumber1 = Math.floor(randomNumber1 * 6) + 1;
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
 
 //generating random number for dice-2
-var randomNumber2 = Math.random();
-randomNumber2 = Math.floor(randomNumber2 * 6) + 1;
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 
 //changing the dice image according to the random number generated
-document.getElementsByClassName(".img1").setAttribute("src", "images/dice" + randomNumber1 + ".png");
-document.getElementsByClassName(".img2").setAttribute("src", "images/dice" + randomNumber2 + ".png");
+document.getElementsByClassName("img1")[0].setAttribute("src", "images/dice" + randomNumber1 + ".png");
+document.getElementsByClassName("img2")[0].setAttribute("src", "images/dice" + randomNumber2 + ".png");
 
 //showing the result of the match
 if (randomNumber1 > randomNumber2) {
